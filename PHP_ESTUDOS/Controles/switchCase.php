f164fecc85060f516e80d887078251474e78d45c
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-03-31 17:50:09 -0300

    novo31_03

---
+++ PHP_ESTUDOS/Controles/switchCase.php
@@ -0,0 +1,39 @@
+<?php
+
+$diaDaSemana = 9; //date("w");
+
+switch ($diaDaSemana) {
+    case 0:
+    echo "Domingo";
+    break;
+    
+    case 1:
+    echo "Segund-Feira";
+    break;
+    
+    case 2:
+    echo "Terça-Feira";
+    break;
+
+    case 3:
+    echo "Quarta-Feira";
+    break;
+
+    case 4:
+    echo "Quinta-Feira";
+    break;
+
+    case 5:
+    echo "Sexta-Feira";
+    break;
+
+    case 6:
+    echo "Sábado";
+    break;
+    
+    default:
+    echo "Data invalida!";
+    break; 
+}
+
+?>
