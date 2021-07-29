d29bb2887c97c49fd4af4f0d262cc5c8c1bafd4b
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-07-28 22:27:58 -0300

    Dados_2

---
+++ JAVASCRIPT_ESTUDOS/inicio/dados_1.js
@@ -0,0 +1,16 @@
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto
+
+console.log("caneta");
+console.log(quantidade)
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+nome = "Caneta Bic";
+console.log(nome);
+
+let nome1 = "Caneta compacto";
+console.log(nome1);
