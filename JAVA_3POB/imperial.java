fbbaf8f799ec29b730c37d7ce2bec379c59748b7
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-09-20 13:08:39 -0300

    novo

---
+++ JAVA_3POB/imperial.java
@@ -0,0 +1,14 @@
+//1. [IMPERIAL] No sistema imperial são utilizadas as seguintes medidas lineares:
+//1 pé = 12 polegadas
+//1 jarda = 3 pés
+//1 milha = 1760 jardas
+//Considerando que 1 polegada equivale a 25,3995 milímetros no sistema métrico, faça o programa que converta um valor informado em centímetros para cada uma das unidades do sistema imperial citadas acima.
+
+
+public class imperial {
+    
+    public static void main(String[] agrs) {
+        double p = 25.3995;
+        System.out.println(p);
+    }
+}
